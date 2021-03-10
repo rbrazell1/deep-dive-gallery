781116f46878fc73724e232f0b256e700201ddab
rbrazell1 <brazellrussell@gmail.com>
2021-03-10 11:58:31 -0700

    added url path

--- app/src/main/java/edu/cnm/deepdive/deepdivegallery/service/DeepDiveGalleryServiceProxy.java
+++ app/src/main/java/edu/cnm/deepdive/deepdivegallery/service/DeepDiveGalleryServiceProxy.java
@@ -3,14 +3,22 @@ package edu.cnm.deepdive.deepdivegallery.service;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import edu.cnm.deepdive.deepdivegallery.BuildConfig;
+import edu.cnm.deepdive.deepdivegallery.model.User;
+import io.reactivex.Single;
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
 import okhttp3.logging.HttpLoggingInterceptor.Level;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
 
 public interface DeepDiveGalleryServiceProxy {
+
+  @GET("users/me")
+  Single<User> getProfile(@Header("Authorization")
+      String bearerToken);
 
   class InstanceHolder {
 
