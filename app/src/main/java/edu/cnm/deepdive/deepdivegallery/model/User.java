89aa237b7dfe4c32e7eea55a765f0fb2940752bc
rbrazell1 <brazellrussell@gmail.com>
2021-03-10 11:49:10 -0700

    Added Proxy and Dependencies

---
+++ app/src/main/java/edu/cnm/deepdive/deepdivegallery/model/User.java
@@ -0,0 +1,55 @@
+package edu.cnm.deepdive.deepdivegallery.model;
+
+
+import com.google.gson.annotations.Expose;
+import java.util.Date;
+import java.util.UUID;
+
+public class User {
+
+  @Expose
+  private UUID id;
+
+  @Expose
+  private Date created;
+
+  @Expose
+  private Date connected;
+
+  @Expose
+  private String displayName;
+
+  // GETTERS AND SETTERS
+
+  public UUID getId() {
+    return id;
+  }
+
+  public void setId(UUID id) {
+    this.id = id;
+  }
+
+  public Date getCreated() {
+    return created;
+  }
+
+  public void setCreated(Date created) {
+    this.created = created;
+  }
+
+  public Date getConnected() {
+    return connected;
+  }
+
+  public void setConnected(Date connected) {
+    this.connected = connected;
+  }
+
+  public String getDisplayName() {
+    return displayName;
+  }
+
+  public void setDisplayName(String displayName) {
+    this.displayName = displayName;
+  }
+}
---
+++ app/src/main/java/edu/cnm/deepdive/deepdivegallery/service/DeepDiveGalleryServiceProxy.java
@@ -0,0 +1,38 @@
+package edu.cnm.deepdive.deepdivegallery.service;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import edu.cnm.deepdive.deepdivegallery.BuildConfig;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import okhttp3.logging.HttpLoggingInterceptor.Level;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public interface DeepDiveGalleryServiceProxy {
+
+  class InstanceHolder {
+
+    private static final DeepDiveGalleryServiceProxy INSTANCE;
+
+    static {
+      Gson gson = new GsonBuilder()
+          .excludeFieldsWithoutExposeAnnotation()
+          .create();
+      HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+      interceptor.setLevel(BuildConfig.DEBUG ? Level.BODY : Level.NONE);
+      OkHttpClient client = new OkHttpClient.Builder()
+          .addInterceptor(interceptor)
+          .build();
+      Retrofit retrofit = new Retrofit.Builder()
+          .addConverterFactory(GsonConverterFactory.create(gson))
+          .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+          .client(client)
+          .baseUrl(BuildConfig.BASE_URL)
+          .build();
+      INSTANCE = retrofit.create(DeepDiveGalleryServiceProxy.class);
+    }
+  }
+
+}
