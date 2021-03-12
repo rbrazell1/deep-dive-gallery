02fd1c2cebe54ceaa62809536167487a15007b04
rbrazell1 <brazellrussell@gmail.com>
2021-03-12 10:04:08 -0700

    starting viewmodel

---
+++ app/src/main/java/edu/cnm/deepdive/deepdivegallery/viewmodel/MainViewModel.java
@@ -0,0 +1,27 @@
+package edu.cnm.deepdive.deepdivegallery.viewmodel;
+
+import android.app.Application;
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.MutableLiveData;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import edu.cnm.deepdive.deepdivegallery.model.User;
+import edu.cnm.deepdive.deepdivegallery.service.UserRepository;
+import org.jetbrains.annotations.NotNull;
+
+public class MainViewModel extends AndroidViewModel {
+
+  private final UserRepository userRepository;
+  private final MutableLiveData<GoogleSignInAccount> account;
+  private final MutableLiveData<User> user;
+  private final MutableLiveData<Throwable> throwable;
+
+
+  public MainViewModel(
+      @NonNull @NotNull Application application) {
+    super(application);
+    userRepository = new UserRepository(application);
+    account = new MutableLiveData<>(userRepository.getAccount);
+
+  }
+}
